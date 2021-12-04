e25f411fee17cb0d5fe0cf4809b07740d613a95a
dindincow 叮叮牛 <dindincow@gmail.com>
2021-12-04 17:08:26 +0800

    累計今日人數

--- imooc-datav-report/src/components/TodayUsers/index.vue
+++ imooc-datav-report/src/components/TodayUsers/index.vue
@@ -1,14 +1,48 @@
 <template>
-    <div class="today-user">
-        <CommonCard/>
-    </div>
+    <CommonCard 
+        title="累计訂單量"
+        value="2,157,420"
+    >
+        <template>
+            <div id="todays-users-chart" :style="{width:'100%',height:'100%'}"></div>
+        </template>
+        <template v-slot:footer>
+            <span>昨日销售额 </span>
+            <span class="emphasis">123456</span>
+        </template>  
+    </CommonCard>  
 </template>
 
 <script>
 import commonCardMixin from '@/mixins/commonCardMixin'
 
 export default {
-    mixins:[commonCardMixin]
+    mixins:[commonCardMixin],
+    mounted(){
+        let chart = this.$echarts.init(document.getElementById('todays-users-chart'));
+       
+        chart.setOption({
+            color:['#3398DB'],
+            xAxis:{
+                type:'category',
+                data:['00:00','00:10','00:20','00:30','00:40','00:50','00:60','00:70','00:80','00:90','00:10','00:20','00:30'],
+            },
+            yAxis:{
+                show:false
+            },
+            series:[{
+                type:'bar',
+                barWidth:'60%',
+                data:[300,400,100,200,150,100,200,300,500,200,400,555,666],
+            }],
+            grid:{
+                top:0,
+                bottom:0,
+                left:0,
+                right:0
+            }
+        })
+    }
 }
 </script>
 <style scoped lang="scss">
--- imooc-datav-report/src/components/TotalUsers/index.vue
+++ imooc-datav-report/src/components/TotalUsers/index.vue
@@ -1,19 +1,76 @@
 <template>
-    <div class="total-user">
-        total-user
-        <CommonCard/>
-    </div>
+    <CommonCard 
+        title="累计用戶數"
+        value="2,157,420"
+    >
+        <template>
+            <div id="total-user-chart" :style="{width:'100%',height:'100%'}"></div>
+        </template>
+        <template v-slot:footer>
+            <div class="total-user-footer">
+                <span>日同比</span>
+                <span class="emphasis">8.73%</span>
+                <div class="increase"></div>
+                <span class="month">月同比</span>
+                <span class="emphasis">5.73%</span>
+                <div class="decrease"></div>
+            </div>
+          
+        </template>  
+    </CommonCard>
 </template>
 
 <script>
 import commonCardMixin from '@/mixins/commonCardMixin'
 
 export default {
-   mixins:[commonCardMixin]
+    mixins:[commonCardMixin],
+    mounted(){
+        let chart = this.$echarts.init(document.getElementById('total-user-chart'));
+       
+        chart.setOption({
+            xAxis:{
+                type:'value',
+                //max:250,
+                
+            },
+            yAxis:{
+                type:'category',
+            },
+            series:[
+                {
+                    type:'bar',
+                    stack:'總量',
+                    data:[120],
+                    barWidth:10,
+                    itemStyle:{
+                        color:'#45c946'
+                    }
+                },{
+                    type:'bar',
+                    stack:'總量',
+                    data:[300],
+                    itemStyle:{
+                        color:'#eee'
+                    }
+                }
+            ],
+            grid:{
+                top:0,
+                bottom:0,
+                left:0,
+                right:0
+            }
+        })
+    }
 }
 </script>
 <style scoped lang="scss">
-.total-sales{
-
+.total-user-footer{
+    display: flex;
+    align-items: center;
+    .month{
+        margin-left: 10px;
+    }
 }
 </style>
